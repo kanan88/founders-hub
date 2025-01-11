eeb20d69352d7050fd4944ffc2d76e53580a4431
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-11 16:18:19 +0100

    implement live update feature for Sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 const Home = async ({
@@ -9,8 +9,8 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -43,6 +43,8 @@ const Home = async ({
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   );
 };
