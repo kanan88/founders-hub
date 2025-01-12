73981ce4f97be24c98bd1e757a4d8fd2ba1b5393
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-12 14:12:07 +0100

    remove console.logs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
@@ -11,10 +10,6 @@ const Home = async ({
 }) => {
   const query = (await searchParams).query;
   const params = { search: query || null };
-
-  const session = await auth();
-
-  console.log(session?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
